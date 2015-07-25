bc68d01ce9dce5dbdf04a491d677dcff1e689fc2
Антон Астафьев <mahairod@astafiev.me>
2015-07-25 19:16:47 +0600

    сборка

--- Source/JNIMorphAPI/build.sh
+++ Source/JNIMorphAPI/build.sh
@@ -1,7 +1,12 @@
 cd `dirname $0`
 export RML=`pwd`/../../
-export JAVA_HOME=/home/keep/java/jdk1.6u23-b05/jdk1.6.0_23
+export JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-1.7.0.9.x86_64
 export JAVA_INCLUDES="-I$JAVA_HOME/include/ -I$JAVA_HOME/include/linux/"
-export PCRE_HOME=/media/wd1/devel/pcre-8.11/pcre-8.11
-export PCRE_OBJECTS_LIB=$PCRE_HOME/.libs
+export PCRE_HOME=/home/mahairod/rpmbuild/BUILDROOT/pcre-8.12-9.fc16.x86_64/usr
+export PCRE_OBJECTS_LIB=$PCRE_HOME/lib64
+
+echo используем Ява в $JAVA_HOME
+echo используем pcre в $PCRE_OBJECTS_LIB
+
 make mode=release libmode=shared
+
